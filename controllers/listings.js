b3f685d36c3fd0caaf1cd44957a27e58999b1fe6
alsaegh98 <abdullajaffer8@gmail.com>
2024-10-23 16:00:39 +0300

    added delete and edit and adding new to the listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,5 +3,74 @@ const router = express.Router()
 
 const Listing = require('../models/listing')
 
+router.get('/', async (req, res) => {
+    try {
+        const populatedListings = await Listing.find({}).populate('owner')
+        console.log('listings: ', populatedListings)
+        res.render('listings/index.ejs' , {
+            listings: populatedListings,
+        })
+    } catch (error) {
+        console.log(error)
+        res.redirect ('/')
+    }
+})
+
+router.get('/new' ,  (req , res) => {
+    
+    res.render('listings/new.ejs')
+})
+
+router.post('/' , async(req , res) =>{
+req.body.owner = req.session.user._id;
+await Listing.create(req.body);
+res.redirect('/');
+})
+
+
+
+router.get('/:listingId', async (req, res) => {
+    try {
+      const populatedListings = await Listing.findById(
+        req.params.listingId
+      ).populate('owner');
+  
+      res.render('listings/show.ejs', {
+        listing: populatedListings,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+
+router.delete('/:listingId', async (req, res) => {
+  try {
+    const listing = await Listing.findById(req.params.listingId);
+    if (listing.owner.equals(req.session.user._id)) {
+      await listing.deleteOne();
+      res.redirect('/listings');
+    } else {
+      res.send("You don't have permission to do that.");
+    }
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/:listingId/edit', async (req, res) => {
+  try {
+    const currentListing = await Listing.findById(req.params.listingId);
+    res.render('listings/edit.ejs', {
+      listing: currentListing,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 
 module.exports = router
+
